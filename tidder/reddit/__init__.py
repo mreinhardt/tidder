c86de81b8651c381df4b6b8f95f9e4fa29bf7260
Mike Reinhardt <mreinhardt@gmail.com>
2013-11-23 17:46:18 -0800

    Disable update checker

--- tidder/reddit/__init__.py
+++ tidder/reddit/__init__.py
@@ -8,5 +8,5 @@ logger = Log().logger
 
 
 user_agent = 'User-Agent: tidder/{0} by mVChr'.format(VERSION)
-api = praw.Reddit(user_agent=user_agent)
+api = praw.Reddit(user_agent=user_agent, disable_update_check=True)
 logger.debug(user_agent)
